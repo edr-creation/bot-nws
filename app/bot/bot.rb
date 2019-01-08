f9a4a02d7c68664bd0e1310a7c87f0264ea214d5
Enzo <dorosario.enzo@protonmail.com>
2019-01-08 11:22:29 +0100

    add recipient

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -22,12 +22,18 @@ end
 Bot.on :postback do |postback|
   if postback.payload == "GET_STARTED_PAYLOAD"
     Bot.deliver({
+      recipient: {
+        id: '45123'
+      },
       message: {
         text: "Bonjour, je suis le bot de la Normandie Web School ! Je peux vous donnez toutes les informations dont vous avez besoin"
       },
       message_type: Facebook::Messenger::Bot::MessagingType::UPDATE
     }, access_token: ENV['ACCESS_TOKEN'])
     Bot.deliver({
+      recipient: {
+        id: '45123'
+      },
       message: {
         text: "Une personne de notre équipe humaine pourra lire vos messages, et ainsi vous répondre si vous avez une demande plus spécifique"
       },
