7263b57ffce01f2a5fd30e44ec08eb56fc034fe3
Enzo <dorosario.enzo@protonmail.com>
2019-01-08 09:39:37 +0100

    Remove subscribe (only thing that work)

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -1,11 +1,6 @@
 require 'facebook/messenger'
 include Facebook::Messenger
 
-Facebook::Messenger::Subscriptions.subscribe(
-  access_token: ENV["ACCESS_TOKEN"],
-  subscribed_fields: %w[feed mention name]
-)
-
 Bot.on :message do |message|
   message.reply(text: 'Bonjour')
 end
