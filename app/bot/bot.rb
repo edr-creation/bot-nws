9826d2a0cba95a3dbf75cedf66a45fe38d626438
Enzo <dorosario.enzo@protonmail.com>
2019-01-08 10:31:14 +0100

    try some things

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -1,6 +1,19 @@
 require 'facebook/messenger'
 include Facebook::Messenger
 
+Facebook::Messenger::Profile.set({
+  greeting: [
+    {
+      locale: 'default',
+      text: 'Welcome to your new bot overlord!'
+    },
+    {
+      locale: 'fr_FR',
+      text: 'Bienvenue dans le bot du Wagon !'
+    }
+  ]
+}, access_token: ENV['ACCESS_TOKEN'])
+
 Bot.on :message do |message|
   message.typing_on
   
