c8b43d4175770161fb2f55b57445cac36f08f847
Enzo <enzo@localhost.localdomain>
2019-01-09 22:07:26 +0100

    Ajout de plusieurs payload

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -24,8 +24,30 @@ Bot.on :postback do |postback|
     postback.reply({
       text: "Bonjour, je suis le bot de la Normandie Web School, je suis la pour vous renseigner toutes les informations dont vous avez besoin"
     })
+
     postback.reply({
-      text: "Les messages seront lus par notre equipe humaine afin de pouvoir repondre a vos questions plus complexes"
+      template_type: 'button',
+      text: "Les messages seront lus par notre equipe humaine afin de pouvoir repondre a vos questions plus complexes",
+      buttons: [
+        { type: 'postback', title: 'Que sais tu faire ?', payload: 'GUIDE_PAYLOAD' }
+      ]
+    })
+  elsif postback.payload == "GUIDE_PAYLOAD"
+    postback.reply({
+      template_type: 'button',
+      text: "Que puis-je faire pour vous ?"
+      buttons: [
+        { type: 'postback', title: 'Prochaine JPO', payload: 'JPO_PAYLOAD' },
+        { type: 'postback', title: 'Enzo', payload: 'ENZO_PAYLOAD' }
+      ]
+    })
+  elsif postback.payload == "JPO_PAYLOAD"
+    postback.reply({
+      text: "La prochaine journee porte ouverte se deroulera le 9 fevrier"
+    })
+  elsif postback.payload == "ENZO_PAYLOAD"
+    postback.reply({
+      text: "Prendre Enzo en stage pourra etre benefique pour votre sante, voir conditions magasins participants"
     })
   end
 end
